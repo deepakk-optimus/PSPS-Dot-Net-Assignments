54c213c453d81b4896bd3ae829dd7cf3a4b64680
Rishab Ilwadi <rishab.ilwadi@optimusinfo.com>
2017-01-18 18:37:19 +0530

    Committing Assignment 9 code

---
+++ RishabAssignments/Assignment9/Induction/Vehicle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Induction
+{
+    class Vehicle
+    {
+        public string make;
+        public int yearOfManufacture;
+        public string model;
+        public float speed;
+
+        public virtual void accelerate()
+        {
+            speed += 10.0f;
+        }
+
+        public virtual void decelerate()
+        {
+            if(speed - 10.0f > 0)
+            {
+                speed -= 10.0f;
+            }
+
+            else
+            {
+                speed = 0.0f;
+            }
+        }
+        
+        public bool isMoving()
+        {
+            bool result = false;
+            if(speed > 0.0f)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        public void printVehicleState()
+        {
+            if(isMoving())
+            {
+                Console.WriteLine("Moving!");
+            }
+
+            else
+            {
+                Console.WriteLine("Its Stationary!");
+            }
+        }
+    }
+}
