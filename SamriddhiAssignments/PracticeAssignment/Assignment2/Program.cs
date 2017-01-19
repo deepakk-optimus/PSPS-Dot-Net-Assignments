3c17eaf1a9e81b57cfb3c56fbe60723ec4e9ef72
samriddhia-optimus <samriddhi.arora@optimusinfo.com>
2017-01-19 10:30:01 +0530

    Committing Assignments

---
+++ SamriddhiAssignments/PracticeAssignment/Assignment2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    /*
+     * Class level comments goes here
+     * 
+     */
+    class TestSwitch
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter preferred language:");
+            string _preferedLanguage = Console.ReadLine();
+
+            switch(_preferedLanguage)
+            {
+                case "VB":
+                case "vb":
+                    Console.WriteLine("VB .NET: OOP, multithreading and more!");
+                    break;
+                case "C#":
+                case "c#":
+                    Console.WriteLine("Good choice, C# is a fine language.");
+                    break;
+                default:
+                    Console.WriteLine("Well...good luck with that!");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SamriddhiAssignments/PracticeAssignment/Assignment3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    enum WeekDays
+    {
+        Sun=3, Mon, Tue, Wed=10, Thu, Fri, Sat
+    };
+
+    class TestEnum
+    {
+        static void Main(string[] args)
+        {
+            var values = Enum.GetNames(typeof(WeekDays));
+
+            foreach(var value in values)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine((int)WeekDays.Sun);
+            Console.ReadKey();
+        }
+    }
+}
