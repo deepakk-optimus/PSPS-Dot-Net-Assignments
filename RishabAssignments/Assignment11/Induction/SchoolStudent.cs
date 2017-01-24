023f6ff99e2fcb84478fee1cff61c9a61f147947
Rishab Ilwadi <rishab.ilwadi@optimusinfo.com>
2017-01-24 18:12:31 +0530

    Committing Thread Assignment

---
+++ RishabAssignments/Assignment11/Induction/SchoolStudent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Induction
+{
+    class SchoolStudent:Student
+    {
+        string _name;
+        string _class;
+        int _enrollmentNo;
+        string[] _hobbies;
+    }
+}
---
+++ RishabAssignments/ThreadAssignment/Induction/ATM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Induction
+{
+    class ATM
+    {
+        private int atmId;
+        //private System.Threading.ReaderWriterLock rwl = new System.Threading.ReaderWriterLock();
+        public static int totalTransaction = 0;
+
+        public int AtmId
+        {
+            get
+            {
+                return atmId;
+            }
+
+            set
+            {
+                atmId = value;
+            }
+        }
+
+        /*public System.Threading.ReaderWriterLock Rwl
+        {
+            get
+            {
+                return rwl;
+            }
+
+        }*/
+    }
+}
